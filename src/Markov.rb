23a37be6da4d90d43d676e6db8e5958fb0ee063c
Nicki Ogg <oggnicki@yahoo.com>
2014-11-25 13:44:13 -0500

    Reading file

--- src/Markov.rb
+++ src/Markov.rb
@@ -6,3 +6,15 @@ puts "Enter file name: "
 filename = gets.chomp
 
 puts "Reading file: #{filename}"
+
+words = []
+
+# Read the file
+
+File.open(filename, "r") do |f|
+	f.each_line do |line|
+		words << line.split(" ")
+	end	
+end
+
+puts words
